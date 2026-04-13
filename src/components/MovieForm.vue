5803afdcb388506013f40877dd4e124f90be56d3
Shevar Roulston <shevarroulston04@gmail.com>
2026-04-13 00:08:08 -0500

    Added Bonus Section

--- src/components/MovieForm.vue
+++ src/components/MovieForm.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="container mt-4">
+    <div v-if="successMessage" class="alert alert-success">
+      {{ successMessage }}
+    </div>
+
+    <div v-if="errors.length > 0" class="alert alert-danger">
+      <ul class="mb-0">
+        <li v-for="(error, index) in errors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
+    </div>
+
     <form
       id="movieForm"
       @submit.prevent="saveMovie"
@@ -47,6 +59,8 @@
 import { ref, onMounted } from "vue";
 
 let csrf_token = ref("");
+let successMessage = ref("");
+let errors = ref([]);
 
 function getCsrfToken() {
   fetch("/api/v1/csrf-token")
@@ -64,6 +78,9 @@ function saveMovie() {
   let movieForm = document.getElementById("movieForm");
   let form_data = new FormData(movieForm);
 
+  successMessage.value = "";
+  errors.value = [];
+
   fetch("/api/v1/movies", {
     method: "POST",
     body: form_data,
@@ -73,6 +90,12 @@ function saveMovie() {
   })
     .then((response) => response.json())
     .then((data) => {
+      if (data.message) {
+        successMessage.value = data.message;
+        movieForm.reset();
+      } else if (data.errors) {
+        errors.value = data.errors;
+      }
       console.log(data);
     })
     .catch((error) => {
